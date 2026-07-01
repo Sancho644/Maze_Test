61b10859bff6c2ea5f06593f6401a86f490f76a8
Alexandr <rfst@mail.ru>
2026-07-01 15:54:57 +0700

    create GameInstaller

---
+++ Assets/Scripts/Installers/GameInstaller.cs
@@ -0,0 +1,33 @@
+﻿using Diamonds;
+using Signals;
+using UnityEngine;
+using Zenject;
+
+namespace Installers
+{
+    public class GameInstaller : MonoInstaller
+    {
+        [SerializeField] private Diamond diamondPrefab;
+        
+        public override void InstallBindings()
+        {
+            SignalBusInstaller.Install(Container);
+
+            Container.DeclareSignal<DiamondCollectedSignal>();
+            Container.DeclareSignal<DiamondsSpawnedSignal>();
+            //Container.DeclareSignal<PlayerDiedSignal>();
+            Container.DeclareSignal<AllDiamondsCollectedSignal>();
+            //Container.DeclareSignal<GameWinSignal>();
+            //Container.DeclareSignal<GameLoseSignal>();
+
+            //Container.BindInterfacesAndSelfTo<GameManager>().AsSingle();
+            Container.BindInterfacesAndSelfTo<DiamondManager>().AsSingle();
+            Container.BindInterfacesAndSelfTo<DiamondSpawner>().AsSingle();
+            //Container.BindInterfacesAndSelfTo<EnemySpawner>().AsSingle();
+            
+            Container.BindFactory<Diamond, Factory>()
+                .FromComponentInNewPrefab(diamondPrefab)
+                .UnderTransformGroup("Diamonds");
+        }
+    }
+}
