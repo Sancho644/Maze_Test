d49b2d2a602133717f74877c4dad170365c4aae5
Alexandr <rfst@mail.ru>
2026-07-02 15:58:23 +0700

    create exit controller

---
+++ Assets/Scripts/ExitController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Diamonds.Signals;
+using Player.Signals;
+using UnityEngine;
+using Zenject;
+
+public class ExitController : MonoBehaviour, IInitializable, IDisposable
+{
+    private SignalBus _signalBus;
+
+    private bool _isUnlocked;
+
+    [Inject]
+    public void Construct(SignalBus signalBus)
+    {
+        _signalBus = signalBus;
+    }
+
+    public void Initialize()
+    {
+        _signalBus.Subscribe<AllDiamondsCollectedSignal>(UnlockExit);
+    }
+
+    public void Dispose()
+    {
+        _signalBus.Unsubscribe<AllDiamondsCollectedSignal>(UnlockExit);
+    }
+
+    private void UnlockExit()
+    {
+        _isUnlocked = true;
+
+        Debug.Log("Exit unlocked");
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!_isUnlocked)
+            return;
+
+        if (!other.CompareTag("Player"))
+            return;
+
+        _signalBus.Fire<PlayerReachedExitSignal>();
+    }
+}
